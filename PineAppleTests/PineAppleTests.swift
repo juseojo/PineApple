2d90e26b4969d2787bd97cc90a28d1571a795c8a
juseojo <j86371224@gmail.com>
2025-07-29 16:53:49 +0900

    Initial Commit

---
+++ PineAppleTests/PineAppleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PineAppleTests.swift
+//  PineAppleTests
+//
+//  Created by seongjun cho on 7/29/25.
+//
+
+import Testing
+@testable import PineApple
+
+struct PineAppleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
